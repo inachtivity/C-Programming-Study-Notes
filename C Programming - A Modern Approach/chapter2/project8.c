c85ff907ef77836b8fe30d53503246dbc14c2077
inachtive <118100704+inachtivity@users.noreply.github.com>
2026-03-03 11:34:02 +0800

    added solution for programming project idea #8 of chapter 2 of C Programming Book by KN King

---
+++ C Programming - A Modern Approach/chapter2/project8.c
@@ -0,0 +1,40 @@
+/*
+* Write a program that calculates the remaining balance on a loan after the first, second, and third monthly payments:
+*
+* Enter amount of loan: 20000.00 
+* Enter interest rate: 6.0 
+* Enter monthly payment: 386.66
+*
+* Balance remaining after first payment: $19713.34 
+* Balance remaining after second payment: $19425.25 
+* Balance remaining after third payment: $19135.71
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    float loanAmount, interestRate, monthlyPayment, balance, withInterest;
+
+    printf("Enter amount of loan: ");
+    scanf("%f", &loanAmount);
+
+    printf("Enter interest rate: ");
+    scanf("%f", &interestRate);
+
+    printf("Enter monthly payment: ");
+    scanf("%f", &monthlyPayment);
+
+
+    balance = (loanAmount - monthlyPayment) + ((loanAmount * (interestRate / 12.00f)) / 100.00f);
+    printf("Balance remaining after first payment: %.2f\n", balance);
+
+    balance = (balance - monthlyPayment) + ((balance * (interestRate / 12.00f)) / 100.00f);
+    printf("Balance remaining after 2nd payment: %.2f\n", balance);
+
+    balance = (balance - monthlyPayment) + ((balance * (interestRate / 12.00f)) / 100.00f);
+    printf("Balance remaining after 3rd payment: %.2f\n", balance);
+
+
+    return 0;
+}
