d21eb0a6e8b4415e4bc7469b5a3b863e04a4fd2b
inachtive <118100704+inachtivity@users.noreply.github.com>
2026-03-06 10:25:12 +0800

    added solution for programming project idea number 2 of chapter 3 of C Programming - A Modern Approach book by KN King

---
+++ C Programming - A Modern Approach/chapter3/project2.c
@@ -0,0 +1,35 @@
+/*
+* Write a program that formats product information entered by the user. A session with the program should look like this:
+*
+* Enter item number: 583 
+* Enter unit price: 13.5
+* Enter purchase date (mm/dd/yyyy): 10/24/2010
+*
+* Item    Unit    Purchase
+*         Price   Date
+* 583     $13.50  10/24/2010
+*
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    int item_num, month, day, year;
+    float unit_price;
+
+    printf("Enter item number: ");
+    scanf("%d", &item_num);
+
+    printf("Enter unit price: ");
+    scanf("%f", &unit_price);
+
+    printf("Enter purchase date: ");
+    scanf("%d / %d / %d", &month, &day, &year);
+
+    printf("\nItem\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n%d\t\t$%.2f\t\t%.2d/%.2d/%d\n", 
+        item_num, unit_price, month, day, year);
+
+    return 0;
+}
