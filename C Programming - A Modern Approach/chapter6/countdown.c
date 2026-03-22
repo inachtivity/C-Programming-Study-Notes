ac578b02d350e0dcbe78933473bf9f6c435a9712
inachtive <118100704+inachtivity@users.noreply.github.com>
2026-03-22 23:48:21 +0800

    added a countdown program using the 'do' statement - Chapter 6 of KN King's C Programming - A Modern Approach book

---
+++ C Programming - A Modern Approach/chapter6/countdown.c
@@ -0,0 +1,17 @@
+/*
+* a countdown using do statement
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    int i = 10;
+
+    do {
+        printf("T minus %d and counting\n", i);
+        i--;
+    } while (i > 0);
+
+    return 0;
+}
