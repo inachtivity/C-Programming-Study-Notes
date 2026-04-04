72f3918dbb186c82bb9bee0491e412f88f41e53b
inachtive <118100704+inachtivity@users.noreply.github.com>
2026-04-04 11:23:08 +0800

    square.c program using for loop, based on chapter 6 of KN King's C Programming book

---
+++ C Programming - A Modern Approach/chapter6/square2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("Enter number of entries in table: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++)
+    {
+        printf("%10d%10d\n", i, i * i);
+    }
+
+    return 0;
+}
